7e3ec2491f38560451424a4601bd426769700aa3
tiunama <tiunama@ESAKA-NOTEBOOK>
2025-03-01 22:42:57 +0800

    首批文件

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+const enterButton = document.querySelector('.enter-button');
+
+enterButton.addEventListener('click', () => {
+  // 在这里添加按钮点击后的行为，例如：
+  alert('Welcome to the Neon City!');
+  // 或者跳转到其他页面：
+  // window.location.href = 'your-next-page.html';
+});
